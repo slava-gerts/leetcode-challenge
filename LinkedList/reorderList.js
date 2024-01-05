69aac42ab286e69402e2ad970db5a491519c9283
Slava Gerts <slava.gerts@invitae.com>
2024-01-06 00:01:54 +0100

    adds reorderList func

---
+++ LinkedList/reorderList.js
@@ -0,0 +1,35 @@
+export function reorderList(head) {
+
+  // find the middle
+  let slow = head
+  let fast = head
+  while (fast.next?.next) {
+    slow = slow.next
+    fast = fast.next.next
+  }
+
+  // reverse the second half
+  let curr = slow.next
+  let prev = null
+  let next = null
+  while (curr) {
+    next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+  }
+
+  // truncate in order to get the first half
+  slow.next = null
+  let h1 = head
+
+  // combine two halves
+  while (prev) {
+    next = h1.next
+    h1.next = prev
+    h1 = prev
+    prev = next
+  }
+
+  return head
+}
--- utils/getTasks.js
+++ utils/getTasks.js
@@ -2,10 +2,13 @@ import {cloneDeep} from './cloneDeep.js'
 
 import {reverseLinkedList, reverseLinkedListRecursion} from '../LinkedList/reverseLinkedList.js'
 import {mergeTwoLists} from '../LinkedList/mergeTwoSortedLists.js'
+import {reorderList} from '../LinkedList/reorderList.js'
+
 import {linkedList, l1, l2} from '../LinkedList/fixtures.js'
 
 export const tasks = new Map([
   ['reverseLinkedList', {func: reverseLinkedList, args: [cloneDeep(linkedList)]}],
   ['reverseLinkedListRecursion', {func: reverseLinkedListRecursion, args: [cloneDeep(linkedList)]}],
   ['mergeTwoSortedLists', {func: mergeTwoLists, args: [cloneDeep(l1), cloneDeep(l2)]}],
+  ['reorderList', {func: reorderList, args: [cloneDeep(linkedList)]}],
 ])
